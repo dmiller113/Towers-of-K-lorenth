1cc46cda7a3816a6e0541321e9cec605e958f9b4
Daniel Miller <danm1113@gmail.com>
2018-03-05 02:10:09 -0800

    Added in initial start room concept

---
+++ src/js/definitions/prefabs/small.js
@@ -0,0 +1,25 @@
+export default {
+    startRoom: {
+        connections: [
+            {x: 3, y: 0},
+            {x: 5, y: 2},
+            {x: 0, y: 4},
+        ],
+        layout: [
+            '***0**',
+            '*##.##',
+            '*#%..0',
+            '*#..%#',
+            '0%...#',
+            '*##.##',
+        ],
+        options: {
+            mirrorX: true,
+            mirrorY: true,
+        },
+        size: {
+            height: 6,
+            width: 6,
+        },
+    }
+}
