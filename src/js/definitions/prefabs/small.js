03e85420ad33937564ce474cd0f4c4eb7fd0cff3
Daniel Miller <danm1113@gmail.com>
2018-03-06 00:44:27 -0800

    corrected start prefab

--- src/js/definitions/prefabs/small.js
+++ src/js/definitions/prefabs/small.js
@@ -2,24 +2,25 @@ export default {
     startRoom: {
         connections: [
             {x: 3, y: 0},
-            {x: 5, y: 2},
-            {x: 0, y: 4},
+            {x: 6, y: 5},
+            {x: 0, y: 5},
         ],
         layout: [
-            '***0**',
-            '*##.##',
-            '*#%..0',
-            '*#..%#',
-            '0%...#',
-            '*##.##',
+            '#######',
+            '#%._.%#',
+            '#o...o#',
+            '#o...o#',
+            '.......',
+            '###.###',
         ],
         options: {
             mirrorX: true,
             mirrorY: true,
+            solid: true,
         },
         size: {
             height: 6,
-            width: 6,
+            width: 7,
         },
     }
 }
